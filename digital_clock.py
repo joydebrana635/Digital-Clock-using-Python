7926bb7c654a01d8b65edb68be8579620bfb4382
Joydeb Rana <77682250+joydebrana635@users.noreply.github.com>
2022-07-22 09:34:43 +0530

    Add files via upload

---
+++ digital_clock.py
@@ -0,0 +1,20 @@
+# Python program to make Digital Clock
+
+import time
+from tkinter import Label
+from tkinter import *
+
+def time_current():
+    t = time.strftime("%I:%M:%S %p")
+    time_display.config(text = t)
+    time_display.after(200, time_current)
+
+tk_window = Tk()
+tk_window.title("My Digital Clock")
+
+time_display = Label(tk_window, font = ("Consolas", 150, "bold"), bg = "black", fg = "red")
+time_display.pack()
+
+time_current()
+
+tk_window.mainloop()
